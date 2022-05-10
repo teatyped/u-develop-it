805d8fcae60497fb090e684e8f4b8dfd680f935e
Khai Truong <kminht@gmail.com>
2022-05-09 23:31:36 -0400

    add CRUD routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const { getConsoleOutput } = require("@jest/console");
 const express = require("express");
 const mysql = require("mysql2");
-const { resourceLimits } = require("worker_threads");
+const inputCheck = require('./utils/inputCheck');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -87,16 +87,32 @@ app.delete('/api/candidate/:id', (req,res) => {
 
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
+app.post('/api/candidate' , ({body}, res) => {
+    
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
 });
+
+
 
 
 // Default response for any other request (Not Found)
