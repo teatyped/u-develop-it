9358376f9122015faee49896d179b83bbf6be09e
Khai Truong <kminht@gmail.com>
2022-05-09 21:18:48 -0400

    connected db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "@Vingtsun1!",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
